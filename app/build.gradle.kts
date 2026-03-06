acd8cd8d634b17107faf7d07d3982dc06e30a1e6
nyakuoff <bertrandbridou1994@gmail.com>
2026-03-06 04:21:17 -0600

    fix build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,8 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
